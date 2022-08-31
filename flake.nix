fcb6cf94196462993f10e69d509e175a88ad2821
Kevin Amado <kamadorueda@gmail.com>
2022-08-31 15:37:55 -0600

    feat: add a dev shell

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+  outputs = inputs: let
+    system = "x86_64-linux";
+    nixpkgs = import inputs.nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = nixpkgs.mkShell {
+      name = "main";
+      packages = [
+        nixpkgs.hugo
+      ];
+    };
+  };
+}
