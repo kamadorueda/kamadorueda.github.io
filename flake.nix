c7fc6c4d49da0362c127a629d5773d09a8f7e9a0
Kevin Amado <kamadorueda@gmail.com>
2024-05-18 13:56:53 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,60 +1,81 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+
+    systems.url = "github:nix-systems/default";
   };
   outputs = inputs: let
-    supportedSystems = inputs.nixpkgs.lib.systems.flakeExposed;
-    forAllSupportedSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+    supportedSystems = import inputs.systems;
 
-    nixpkgsForSystem = system: import inputs.nixpkgs {inherit system;};
-    nixpkgs = forAllSupportedSystems nixpkgsForSystem;
+    forEachSystem = inputs.nixpkgs.lib.genAttrs supportedSystems;
 
-    devShellsForSystem = system:
-      with nixpkgs.${system}; {
-        default = mkShell {
+    nixkpgsOverlayForSystem = system: (nixpkgs: _: rec {
+      kamadorueda = {
+        ci = nixpkgs.writeShellApplication {
+          name = "ci";
+          runtimeInputs = [nixpkgs.nodejs];
+          text = ''
+            npm run format
+            npm run lint -- --fix
+            npm run lint
+          '';
+        };
+
+        frontend = nixpkgs.writeShellApplication {
+          name = "frontend";
+          runtimeInputs = [nixpkgs.nodejs];
+          text = ''
+            npm run dev
+          '';
+        };
+
+        deploy = nixpkgs.writeShellApplication {
+          name = "deploy";
+          runtimeInputs = [nixpkgs.nodejs];
+          text = ''
+            npm run build
+            npm run gh-pages -- -d dist
+          '';
+        };
+
+        resize = nixpkgs.writeShellApplication {
+          name = "resize";
+          runtimeInputs = [nixpkgs.imagemagick];
+          text = ''
+            name="''${1%.*}"
+            ext="''${1##*.}"
+            for size in "''${@:2}"; do
+              magick "$1" -resize "$size"  "$name$size.$ext"
+            done
+          '';
+        };
+
+        shell = nixpkgs.mkShell {
           name = "default";
           packages = [
-            nodejs
-            mprocs
-            (writeShellApplication {
-              name = "ci";
-              runtimeInputs = [ nodejs ];
-              text = ''
-                npm run format
-                npm run lint -- --fix
-                npm run lint
-              '';
-            })
-            (writeShellApplication {
-              name = "frontend";
-              runtimeInputs = [ nodejs ];
-              text = ''
-                npm run dev
-              '';
-            })
-            (writeShellApplication {
-              name = "deploy";
-              runtimeInputs = [ nodejs ];
-              text = ''
-                npm run build
-                npm run gh-pages -- -d dist
-              '';
-            })
-            (writeShellApplication {
-              name = "resize";
-              runtimeInputs = [ imagemagick ];
-              text = ''
-                name="''${1%.*}"
-                ext="''${1##*.}"
-                for size in "''${@:2}"; do
-                  magick "$1" -resize "$size"  "$name$size.$ext"
-                done
-              '';
-            })
+            kamadorueda.ci
+            kamadorueda.deploy
+            kamadorueda.frontend
+            kamadorueda.resize
+            nixpkgs.nodejs
+            nixpkgs.mprocs
           ];
         };
       };
+    });
+
+    nixpkgsForSystem = system:
+      import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          (nixkpgsOverlayForSystem system)
+        ];
+      };
+
+    nixpkgs = forEachSystem nixpkgsForSystem;
   in {
-    devShells = forAllSupportedSystems devShellsForSystem;
+    devShells = forEachSystem (system: {
+      default = nixpkgs.${system}.kamadorueda.shell;
+    });
   };
 }
